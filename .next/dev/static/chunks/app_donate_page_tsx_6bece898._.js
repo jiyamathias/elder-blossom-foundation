476b03e2b69cda18344845727a78c09ec20edd74
jiyamathias <mathiasjiya2@gmail.com>
2026-01-02 14:42:38 +0100

    .next added to gitignore

--- .next/dev/static/chunks/app_donate_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_70eceadf._.js",
-  "static/chunks/components_bbb1403d._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_28215cbb._.js",
-  "static/chunks/components_0d88cb9e._.js"
-],
-    source: "dynamic"
-});
